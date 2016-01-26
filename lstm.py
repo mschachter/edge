43022aad3706d86c8956e448e07c173ff9f32aa8
Guy Isely <gisely@gmail.com>
2016-01-25 18:02:19 -0800

    first draft model + optimization code

--- lstm.py
+++ lstm.py
@@ -1,3 +1,38 @@
 class LSTM_Layer(object):
-    def __init__(self, u_init):
-        pass
+    # Following the closest to the notation of Greff 2015
+
+    def __init__(self, n_input, n_unit):
+        ## Layer parameters: W input weights, R recurrent weights, b bias
+
+        # Block input
+        self.Wz = tf.Variable(tf.truncated_normal([n_input, n_unit], 0.0, 0.1))
+        self.Rz = tf.Variable(tf.truncated_normal([n_unit, n_unit], 0.0, 0.1))
+        self.bz = tf.Variable(tf.zeros([1, n_unit]))
+
+        # Input gate
+        self.Wi = tf.Variable(tf.truncated_normal([n_input, n_unit], 0.0, 0.1))
+        self.Ri = tf.Variable(tf.truncated_normal([n_unit, n_unit], 0.0, 0.1))
+        self.bi = tf.Variable(tf.zeros([1, n_unit]))
+
+        # Forget gate
+        self.Wf = tf.Variable(tf.truncated_normal([n_input, n_unit], 0.0, 0.1))
+        self.Rf = tf.Variable(tf.truncated_normal([n_unit, n_unit], 0.0, 0.1))
+        # using a positive bias suggested Jozefowicz 2015
+        self.bf = tf.Variable(tf.ones([1, n_unit]))
+
+        self.c = None
+
+    def set_state(self, state):
+        self.c = state
+
+
+    def step(self, x, y):
+        """Updates the internal memory state and returns the output"""
+
+        z = tf.sigmoid(tf.matmul(x, self.Wz) + tf.matmul(y, self.Rz) + self.bz)
+        i = tf.sigmoid(tf.matmul(x, self.Wi) + tf.matmul(y, self.Ri) + self.bi)
+        f = tf.sigmoid(tf.matmul(x, self.Wf) + tf.matmul(y, self.Rf) + self.bf)
+
+        c = i*z + f*self.c
+
+        return tf.tanh(self.c)
--- train.py
+++ train.py
@@ -1,15 +1,103 @@
 import tensorflow as tf
 import InputGenerator
 import lstm
+import yaml
+import time
+from datetime import datetime
+import sys
 
-filename = 'there_is_no_file_yet.txt'
+t_run_state = time.time()
 
-input_generator = Input_Generator(filname, batch_size, n_bptt)
+if len(sys.argv) > 1:
+    param_file_path = sys.argv[1]
+else:
+    param_file_path = 'params/basic_text_lstm.yaml'
+
+print 'Running on', param_file_path
+
+with open(param_file_path, 'r') as param_file:
+    hparams = yaml.load(param_file)
+
+print '-------------'
+print hparams
+print '-------------'
+
+####################### TODO: where I left off
+
+input_generator = Input_Generator(train_text, batch_size, n_bptt)
+n_alphabet = input_generator.n_alphabet
 
 n_unit = 64
 
 graph = tf.Graph()
 with graph.as_default():
+
+    ## Create the graph input and state variables
+
+    # Input nodes
+    xs = [tf.placeholder(tf.float32, shape=[batch_size, n_alphabet]) for
+        _ in xrange(n_bptt + 1)]
+    x_inputs = xs[:n_bptt]
+    x_labels = xs[1:]
+
     lstm_layer = lstm.LSTM_Layer(num_nodes)
 
-    
+    # Ouput weights and biases
+    Wo = tf.Variable(tf.truncated_normal([n_unit, n_alphabet], 0.0, 0.1))
+    bo = tf.Variable(tf.zeros([n_alphabet]))
+
+    # Used to carry over the network state between forward propagations
+    saved_y = tf.Variable(tf.zeros([batch_size, n_unit]), trainable=False)
+    saved_state = tf.Variable(tf.zeros([batch_size, n_unit]), trainable=False)
+
+
+    ## Build the forward propagation graph
+    y = saved_y
+    lstm_layer.set_state(saved_state)
+
+    ys = list()
+    for x in x_inputs:
+        y = lstm_layer.step(x, y)
+        ys.append(y)
+
+    with tf.control_dependencies([saved_y.assign(y),
+        save_state.assign(lstm_layer.c)]):
+
+        x_predictions = tf.nn.xw_plus_b(tf.concat(0, ys), Wo, bo)
+        prediction_error = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+            x_predictions, tf.concat(0, x_labels)
+        ))
+
+
+    ## Build the optimizer
+    t = tf.Variable(0) # the step variable
+    eta = tf.train.exponential_decay(10.0, t, 5000, 0.1, staircase=True)
+    optimizer = tf.train.GradientDescentOptimizer(eta)
+    grads, params = zip(*optimizer.compute_gradients(prediction_error))
+    grads, _ = tf.clip_by_global_norm(grads, 1)
+    apply_grads = optimizer.apply_gradients(zip(grads, params), global_step=t)
+
+num_steps = 7001 # cause why the fuck not
+summary_freq = 100
+mean_error = 0.0
+
+with tf.Session(graph=graph):
+    tf.initialize_all_variables().run()
+
+    for step in xrange(num_steps):
+
+        # Set up input value -> input var mapping
+        batches = input_generator.next_batches()
+        feed_dict = dict()
+        for i in xrange(n_bptt + 1):
+            feed_dict[xs[i]] = batches[i]
+
+        _, error_val, eta_val = session.run(
+            [apply_grads, prediction_error, eta], feed_dict=feed_dict
+        )
+        mean_error += error_val
+
+        if step % summary_frequency == 0 && step > 0:
+            mean_error = mean_error/summary_freq
+            print 'Average error at step', step, ':', mean_error, 'learning rate:', eta_val
+            mean_error = 0.0
