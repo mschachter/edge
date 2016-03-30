99dc4fdcf32f56ce348598170db6fe81c89eca3f
Guy Isely <gisely@gmail.com>
2016-03-29 17:15:41 -0700

    simplifying

--- edge/layers.py
+++ edge/layers.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import numpy as np
 
 def init_weights(n_input, n_unit, hparams):
 
@@ -13,8 +12,8 @@ class Linear_Layer(object):
         self.n_input = n_input
         self.n_unit = n_output
 
-        self.W = tf.Variable(init_weights(n_input, n_output, hparams), name= 'W')
-        self.b = tf.Variable(tf.zeros([n_output]), name = 'b')
+        self.W = tf.Variable(init_weights(n_input, n_output, hparams), name='W')
+        self.b = tf.Variable(tf.zeros([n_output]), name='b')
 
     def output(self, x):
         return tf.nn.xw_plus_b(x, self.W, self.b)
@@ -26,23 +25,15 @@ class SRNN_Layer(object):
         self.n_input = n_input
         self.n_unit = n_unit
 
-        self.W = tf.Variable(init_weights(n_input, n_unit, hparams), name = 'W')
-        self.R = tf.Variable(init_weights(n_unit, n_unit, hparams), name = 'R')
-        self.b = tf.Variable(tf.zeros([1, n_unit]), name = 'b')
+        self.W = tf.Variable(init_weights(n_input, n_unit, hparams), name='W')
+        self.R = tf.Variable(init_weights(n_unit, n_unit, hparams), name='R')
+        self.b = tf.Variable(tf.zeros([1, n_unit]), name='b')
 
-    def get_new_states(self, n_state):
-        new_h = tf.Variable(tf.zeros([n_state, self.n_unit]), trainable=False, name = 'h')
-        return new_h,
-
-    def step(self, state, x, *d_state):
+    def step(self, h, x, *d_state):
         """Updates returns the state updated by input x"""
-        h = state[0]
         h = tf.tanh(tf.matmul(x, self.W) + tf.matmul(h, self.R) + self.b)
 
-        return h,
-
-    def gradient(self, error, state):
-        return tf.gradients(error, state[0])
+        return h
 
 
 class EDSRNN_Layer(SRNN_Layer):
@@ -50,17 +41,14 @@ class EDSRNN_Layer(SRNN_Layer):
     def __init__(self, n_input, n_unit, hparams):
         super(EDSRNN_Layer, self).__init__(n_input, n_unit, hparams)
 
-        self.E = tf.Variable(init_weights(n_unit, n_unit, hparams), name = 'E')
+        self.E = tf.Variable(init_weights(n_unit, n_unit, hparams), name='E')
 
-    def step(self, state, x, *d_state):
+    def step(self, h, x, *d_state):
         """Updates returns the state updated by input x"""
-        dh = d_state[0][0]
-        h = state[0]
-        # TODO: should there be a non-linearity on the gradient here?????
-        state = tf.sigmoid(tf.matmul(x, self.W) + tf.matmul(h, self.R)
+        dh = d_state[0]
+        h = tf.sigmoid(tf.matmul(x, self.W) + tf.matmul(h, self.R)
             + tf.matmul(dh, self.E) + self.b)
-
-        return state,
+        return h
 
 class GRU_Layer(object):
     def __init__(self, n_input, n_unit, hparams):
@@ -68,29 +56,24 @@ class GRU_Layer(object):
         self.n_unit = n_unit
 
         # The computed update
-        with tf.name_scope('update') as scope:
-            self.Wu = tf.Variable(init_weights(n_input, n_unit, hparams), name = 'Wu')
-            self.Ru = tf.Variable(init_weights(n_unit, n_unit, hparams), name = 'Ru')
-            self.bu = tf.Variable(tf.zeros([1, n_unit]), name = 'bu')
+        with tf.name_scope('update'):
+            self.Wu = tf.Variable(init_weights(n_input, n_unit, hparams), name='Wu')
+            self.Ru = tf.Variable(init_weights(n_unit, n_unit, hparams), name='Ru')
+            self.bu = tf.Variable(tf.zeros([1, n_unit]), name='bu')
 
         # The reset gate
-        with tf.name_scope('reset_gate') as scope:
-            self.Wr = tf.Variable(init_weights(n_input, n_unit, hparams), name = 'Wr')
-            self.Rr = tf.Variable(init_weights(n_unit, n_unit, hparams), name = 'Rr')
-            self.br = tf.Variable(tf.zeros([1, n_unit]), name = 'br')
+        with tf.name_scope('reset_gate'):
+            self.Wr = tf.Variable(init_weights(n_input, n_unit, hparams), name='Wr')
+            self.Rr = tf.Variable(init_weights(n_unit, n_unit, hparams), name='Rr')
+            self.br = tf.Variable(tf.zeros([1, n_unit]), name='br')
 
         # The update gate
-        with tf.name_scope('update_gate') as scope:
-            self.Wz = tf.Variable(init_weights(n_input, n_unit, hparams), name = 'Wz')
-            self.Rz = tf.Variable(init_weights(n_unit, n_unit, hparams), name = 'Rz')
-            self.bz = tf.Variable(tf.zeros([1, n_unit]), name = 'bz')
+        with tf.name_scope('update_gate'):
+            self.Wz = tf.Variable(init_weights(n_input, n_unit, hparams), name='Wz')
+            self.Rz = tf.Variable(init_weights(n_unit, n_unit, hparams), name='Rz')
+            self.bz = tf.Variable(tf.zeros([1, n_unit]), name='bz')
 
-    def get_new_states(self, n_state):
-        new_h = tf.Variable(tf.zeros([n_state, self.n_unit]), trainable=False, name = 'h')
-        return new_h,
-
-    def step(self, state, x, *d_state):
-        h = state[0]
+    def step(self, h, x, *d_state):
 
         r = tf.sigmoid(tf.matmul(x, self.Wr) + tf.matmul(h, self.Rr) + self.br)
         z = tf.sigmoid(tf.matmul(x, self.Wz) + tf.matmul(h, self.Rz) + self.bz)
@@ -99,93 +82,32 @@ class GRU_Layer(object):
 
         h = (1.0 - z)*h + z*h_tilde
 
-        return h,
-
-    def gradient(self, error, state):
-        return tf.gradients(error, state[0])
+        return h
 
 class EDGRU_Layer(GRU_Layer):
     def __init__(self, n_input, n_unit, hparams):
         super(EDGRU_Layer, self).__init__(n_input, n_unit, hparams)
 
-        with tf.name_scope('update') as scope:
-            self.Eu = tf.Variable(init_weights(n_unit, n_unit, hparams), name = 'Eu')
-        with tf.name_scope('reset_gate') as scope:
-            self.Er = tf.Variable(init_weights(n_unit, n_unit, hparams), name = 'Er')
-        with tf.name_scope('update_gate') as scope:
-            self.Ez = tf.Variable(init_weights(n_unit, n_unit, hparams), name = 'Ez')
+        with tf.name_scope('update'):
+            self.Eu = tf.Variable(init_weights(n_unit, n_unit, hparams), name='Eu')
+        with tf.name_scope('reset_gate'):
+            self.Er = tf.Variable(init_weights(n_unit, n_unit, hparams), name='Er')
+        with tf.name_scope('update_gate'):
+            self.Ez = tf.Variable(init_weights(n_unit, n_unit, hparams), name='Ez')
 
-    def step(self, state, x, *d_state):
-        h = state[0]
-        dh = d_state[0][0]
+    def step(self, h, x, *d_state):
+        dh = d_state[0]
 
         # import ipdb; ipdb.set_trace()
 
-        r = tf.sigmoid(tf.matmul(x, self.Wr) + tf.matmul(h, self.Rr) + tf.matmul(dh, self.Er) + self.br)
-        z = tf.sigmoid(tf.matmul(x, self.Wz) + tf.matmul(h, self.Rz) + tf.matmul(dh, self.Ez) + self.bz)
+        r = tf.sigmoid(tf.matmul(x, self.Wr) + tf.matmul(h, self.Rr)
+            + tf.matmul(dh, self.Er) + self.br)
+        z = tf.sigmoid(tf.matmul(x, self.Wz) + tf.matmul(h, self.Rz)
+            + tf.matmul(dh, self.Ez) + self.bz)
 
-        h_tilde = tf.tanh(tf.matmul(x, self.Wu) + r*(tf.matmul(h, self.Ru) + tf.matmul(dh, self.Eu)) + self.bu)
+        h_update = tf.tanh(tf.matmul(x, self.Wu) + r*(tf.matmul(h, self.Ru)
+            + tf.matmul(dh, self.Eu)) + self.bu)
 
-        h = (1.0 - z)*h + z*h_tilde
+        h = (1.0 - z)*h + z*h_update
 
-        return h,
-
-class LSTM_Layer(object):
-
-
-    def __init__(self, n_input, n_unit, hparams):
-        self.n_input = n_input
-        self.n_unit = n_unit
-
-        ## Layer parameters: W input weights, R recurrent weights, b bias
-
-        # The computed update
-        with tf.name_scope('update') as scope:
-            self.Wu = tf.Variable(init_weights(n_input, n_unit, hparams), name = 'Wu')
-            self.Ru = tf.Variable(init_weights(n_unit, n_unit, hparams), name = 'Ru')
-            self.bu = tf.Variable(tf.zeros([1, n_unit]), name = 'bu')
-
-        # Input gate
-        with tf.name_scope('input_gate') as scope:
-            self.Wi = tf.Variable(init_weights(n_input, n_unit, hparams), name = 'Wi')
-            self.Ri = tf.Variable(init_weights(n_unit, n_unit, hparams), name = 'Ri')
-            self.bi = tf.Variable(tf.zeros([1, n_unit]), name = 'bi')
-
-        # Forget gate
-        with tf.name_scope('forget_gate') as scope:
-            self.Wf = tf.Variable(init_weights(n_input, n_unit, hparams), name = 'Wf')
-            self.Rf = tf.Variable(init_weights(n_unit, n_unit, hparams), name = 'Rf')
-            # using a positive bias as suggested in Joxefowicx 2015
-            self.bf = tf.Variable(tf.ones([1, n_unit]), name = 'bf')
-
-        # Output gate
-        with tf.name_scope('output_gate') as scope:
-            self.Wo = tf.Variable(init_weights(n_input, n_unit, hparams), name = 'Wo')
-            self.Ro = tf.Variable(init_weights(n_unit, n_unit, hparams), name = 'Ro')
-            self.bo = tf.Variable(tf.zeros([1, n_unit]), name = 'bo')
-
-    def get_new_states(self, n_state):
-        new_h = tf.Variable(tf.zeros([n_state, self.n_unit]), trainable=False, name = 'h')
-        new_c = tf.Variable(tf.zeros([n_state, self.n_unit]), trainable=False, name = 'c')
-
-        return new_h, new_c
-
-
-    def step(self, state, x, *d_state):
-        h, c = state
-        """Updates returns the state updated by input x"""
-        u = tf.sigmoid(tf.matmul(x, self.Wu) + tf.matmul(h, self.Ru) + self.bu)
-        i = tf.sigmoid(tf.matmul(x, self.Wi) + tf.matmul(h, self.Ri) + self.bi)
-        f = tf.sigmoid(tf.matmul(x, self.Wf) + tf.matmul(h, self.Rf) + self.bf)
-        o = tf.sigmoid(tf.matmul(x, self.Wo) + tf.matmul(h, self.Ro) + self.bo)
-
-        c = i*u + f*c
-        h = o*tf.tanh(c)
-
-        return h, c
-
-    def gradient(self, error, state):
-        # Using the memory state since the output state is subservient to it
-        # but who knows
-        _, c = state
-        return tf.gradients(error, c)
+        return h
--- edge/networks.py
+++ edge/networks.py
@@ -9,47 +9,49 @@ class Basic_Network(object):
         self.uses_error = False
 
         if hparams['rnn_type'] == 'SRNN':
-            with tf.name_scope('srnn_layer') as scope:
+            with tf.name_scope('srnn_layer'):
                 self.rnn_layer = layers.SRNN_Layer(n_input, n_unit, hparams)
         elif hparams['rnn_type'] == 'LSTM':
-            with tf.name_scope('lstm_layer') as scope:
+            with tf.name_scope('lstm_layer'):
                 self.rnn_layer = layers.LSTM_Layer(n_input, n_unit, hparams)
         elif hparams['rnn_type'] == 'GRU':
-            with tf.name_scope('gru_layer') as scope:
+            with tf.name_scope('gru_layer'):
                 self.rnn_layer = layers.GRU_Layer(n_input, n_unit, hparams)
         elif hparams['rnn_type'] == 'EDSRNN':
-            with tf.name_scope('edsrnn_layer') as scope:
+            with tf.name_scope('edsrnn_layer'):
                 self.rnn_layer = layers.EDSRNN_Layer(n_input, n_unit, hparams)
                 self.uses_error = True
         elif hparams['rnn_type'] == 'EDGRU':
-            with tf.name_scope('edsrnn_layer') as scope:
+            with tf.name_scope('edsrnn_layer'):
                 self.rnn_layer = layers.EDGRU_Layer(n_input, n_unit, hparams)
                 self.uses_error = True
 
-        with tf.name_scope('logit_layer') as scope:
+        with tf.name_scope('logit_layer'):
             self.logit_layer = layers.Linear_Layer(n_unit, n_input, hparams)
 
 
     def step(self, state, x_input, *d_state):
         state = self.rnn_layer.step(state, x_input, *d_state)
-        output = self.logit_layer.output(state[0])
+        output = self.logit_layer.output(state)
         return state, output
 
     def gradient(self, error, state):
         '''Computes the gradient of the error with respect to the network state'''
-        return self.rnn_layer.gradient(error, state)
+        return tf.gradients(error, state)[0]
 
     def get_new_states(self, n_state):
-        return self.rnn_layer.get_new_states(n_state)
+        return tf.Variable(tf.zeros([n_state, self.rnn_layer.n_unit]), trainable=False, name='h')
 
     # Records current state of the network in storage
     def store_state_op(self, state, storage):
-        store_ops = [store_var.assign(state_var)
-            for state_var, store_var in zip(state, storage)]
-        return tf.group(*store_ops)
+        return storage.assign(state)
+        # store_ops = [store_var.assign(state_var)
+        #     for state_var, store_var in zip(state, storage)]
+        # return tf.group(*store_ops)
 
     # Resets the network state to zero
     def reset_state_op(self, state):
-        reset_ops  = [state_var.assign(tf.zeros(state_var.get_shape()))
-            for state_var in state]
-        return tf.group(*reset_ops)
+        return state.assign(tf.zeros(state.get_shape()))
+        # reset_ops  = [state_var.assign(tf.zeros(state_var.get_shape()))
+        #     for state_var in state]
+        # return tf.group(*reset_ops)
